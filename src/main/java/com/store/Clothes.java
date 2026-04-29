e92d67a6e4a8bfcd5c553e9a6d928e16a11f0ada
navchanya662-ops <navchanya662@gmail.com>
2026-04-29 08:22:15 +0300

    Add Clothes class

---
+++ src/main/java/com/store/Clothes.java
@@ -0,0 +1,78 @@
+package com.store;
+
+import java.util.Objects;
+
+public class Clothes {
+    private String name;
+    private String size;
+    private String color;
+    private double price;
+
+    public Clothes(String name, String size, String color, double price) {
+        this.name = name;
+        this.size = size;
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Одяг{"
+                + "назва='" + name + '\''
+                + ", розмір='" + size + '\''
+                + ", колір='" + color + '\''
+                + ", ціна=" + price
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Clothes clothes)) {
+            return false;
+        }
+        return Double.compare(price, clothes.price) == 0
+                && Objects.equals(name, clothes.name)
+                && Objects.equals(size, clothes.size)
+                && Objects.equals(color, clothes.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, size, color, price);
+    }
+}
